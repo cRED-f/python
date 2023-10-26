f8eabd8f1bf7f166c001bf203634d243a53afbc9
cRED-f <mrislam.fahim@gmail.com>
2023-10-26 22:33:48 +0600

    learning

--- pythonDjango/first_project/firstApp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import  Topic,AccessRecord,Webpage
- # Register your models here.
-admin.site.register(AccessRecord)
-admin.site.register(Topic)
-admin.site.register(Webpage)
--- pythonProject/main.py
+++
@@ -1,5 +0,0 @@
-import math
-
-a=math.sin(90)
-b=math.radians(a)
-print(b)
